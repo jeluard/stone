8a544f5345c2ce0ffe86e3ffd461d7bf2ed5f618
Julien Eluard <julien.eluard@gmail.com>
2013-01-13 15:29:41 -0300

    Added min consolidator.

---
+++ core/src/main/java/com/github/jeluard/stone/impl/MinConsolidator.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright 2012 Julien Eluard
+ * This project includes software developed by Julien Eluard: https://github.com/jeluard/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.jeluard.stone.impl;
+
+import com.github.jeluard.stone.spi.Consolidator;
+
+public class MinConsolidator implements Consolidator {
+
+  private static final int DEFAULT_VALUE = Integer.MAX_VALUE;
+  private int currentMin = MinConsolidator.DEFAULT_VALUE;
+
+  @Override
+  public synchronized void accumulate(final long timestamp, final int value) {
+    if (value < this.currentMin) {
+      this.currentMin = value;
+    }
+  }
+
+  @Override
+  public int consolidateAndReset() {
+    try {
+      return this.currentMin;
+    } finally {
+      this.currentMin = MinConsolidator.DEFAULT_VALUE;
+    }
+  }
+
+}
