5e5acdbabff5e6d2808be63f5463149bafc1931f
Julien Eluard <julien.eluard@gmail.com>
2013-01-15 23:44:17 -0300

    Improved JournalIOStorage defaults.

--- implementations/storages/journalio/src/main/java/com/github/jeluard/stone/impl/JournalIOStorageFactory.java
+++ implementations/storages/journalio/src/main/java/com/github/jeluard/stone/impl/JournalIOStorageFactory.java
@@ -17,12 +17,22 @@
 package com.github.jeluard.stone.impl;
 
 import com.github.jeluard.stone.api.Archive;
+import com.github.jeluard.stone.spi.Consolidator;
 import com.github.jeluard.stone.spi.Storage;
 import com.github.jeluard.stone.spi.StorageFactory;
+import com.google.common.base.Function;
+import com.google.common.base.Joiner;
+import com.google.common.base.Optional;
 import com.google.common.base.Preconditions;
+import com.google.common.collect.Collections2;
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Collection;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 
 import journal.io.api.Journal;
 import journal.io.api.RecoveryErrorHandler;
@@ -32,23 +42,107 @@ import journal.io.api.RecoveryErrorHandler;
  */
 public class JournalIOStorageFactory implements StorageFactory {
 
-  protected Journal createJournal(final Archive archive) throws IOException {
+  private static final String WRITER_THREADS_NAME_FORMAT = "Stone JournalIO-Writer #{0}";
+  private static final String DISPOSER_THREADS_NAME_FORMAT = "Stone JournalIO-Disposer";
+  private static final String CONSOLIDATOR_SUFFIX = "Consolidator";
+
+  private final ScheduledExecutorService disposerScheduledExecutorService;
+  private final Executor writerExecutor;
+
+  public JournalIOStorageFactory() {
+    this(Executors.newFixedThreadPool(2*Runtime.getRuntime().availableProcessors(), new ThreadFactoryBuilder().setDaemon(true).setNameFormat(JournalIOStorageFactory.WRITER_THREADS_NAME_FORMAT).build()),
+         Executors.newSingleThreadScheduledExecutor(new ThreadFactoryBuilder().setDaemon(true).setNameFormat(JournalIOStorageFactory.DISPOSER_THREADS_NAME_FORMAT).build()));
+  }
+
+  public JournalIOStorageFactory(final Executor writerExecutor, final ScheduledExecutorService disposerScheduledExecutorService) {
+    this.writerExecutor = Preconditions.checkNotNull(writerExecutor, "null writerExecutor");
+    this.disposerScheduledExecutorService = Preconditions.checkNotNull(disposerScheduledExecutorService, "null disposerScheduledExecutorService");
+  }
+
+  protected String mainDirectory(final String id, final Archive archive) {
+    return "stone-journal";
+  }
+
+  /**
+   * @param consolidators
+   * @return all consolidators identifiers (MaxConsolidator => max)
+   */
+  protected final Collection<String> extractConsolidatorIdentifiers(final Collection<Consolidator> consolidators) {
+    return Collections2.transform(consolidators, new Function<Consolidator, String>() {
+      @Override
+      public String apply(final Consolidator input) {
+        final String simpleName = input.getClass().getSimpleName();
+        if (simpleName.endsWith(JournalIOStorageFactory.CONSOLIDATOR_SUFFIX)) {
+          return simpleName.substring(0, simpleName.length()-JournalIOStorageFactory.CONSOLIDATOR_SUFFIX.length()).toLowerCase();
+        }
+        return simpleName;
+      }
+    });
+  }
+
+  /**
+   * @param id
+   * @param archive
+   * @return an optional prefix used when creating file names
+   */
+  protected Optional<String> filePrefix(final String id, final Archive archive) {
+    final Collection<String> consolidatorIdentifiers = extractConsolidatorIdentifiers(archive.getConsolidators());
+    return Optional.of(Joiner.on("-").join(consolidatorIdentifiers));
+  }
+
+  /**
+   * @param id
+   * @param archive
+   * @return an optional suffix used when creating file names
+   */
+  protected Optional<String> fileSuffix(final String id, final Archive archive) {
+    return Optional.absent();
+  }
+
+  /**
+   * @param id
+   * @param archive
+   * @return an initialized {@link Journal} dedicated to this {@link id} / {@link archive} pair
+   * @throws IOException 
+   */
+  protected Journal createJournal(final String id, final Archive archive) throws IOException {
     final Journal journal = new Journal();
-    final File file = new File("stone-journal");
-    file.mkdir();
+    final String mainDirectory = mainDirectory(id, archive);
+    final File file = new File(mainDirectory);
+    if (!file.isDirectory()) {
+      throw new IllegalArgumentException("Main directory <"+mainDirectory+"> is not a directory");
+    }
+    if (!file.mkdirs()) {
+      throw new IllegalArgumentException("Failed to create main directory <"+mainDirectory+">");
+    }
     journal.setDirectory(file);
+    final Optional<String> filePrefix = filePrefix(id, archive);
+    if (filePrefix.isPresent()) {
+      journal.setFilePrefix(filePrefix.get());
+    }
+    final Optional<String> fileSuffix = fileSuffix(id, archive);
+    if (fileSuffix.isPresent()) {
+      journal.setFileSuffix(fileSuffix.get());
+    }
+    journal.setArchiveFiles(false);
+    journal.setChecksum(true);
     journal.setRecoveryErrorHandler(RecoveryErrorHandler.ABORT);
     journal.setPhysicalSync(true);
+    //journal.setMaxFileLength(maxFileLength);
+    //journal.setMaxWriteBatchSize(maxWriteBatchSize);
+    journal.setWriter(this.writerExecutor);
+    //journal.setDisposeInterval()
+    journal.setDisposer(this.disposerScheduledExecutorService);
     journal.open();
     return journal;
   }
 
   @Override
-  public Storage createOrOpen(final String id, final Archive archive) throws IOException {
+  public final Storage createOrOpen(final String id, final Archive archive) throws IOException {
     Preconditions.checkNotNull(id, "null id");
     Preconditions.checkNotNull(archive, "null archive");
 
-    return new JournalIOStorage(createJournal(archive));
+    return new JournalIOStorage(createJournal(id, archive));
   }
 
 }
