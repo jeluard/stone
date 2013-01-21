b4c4878f3b9ffbe1320168f31b2480a826779db4
Julien Eluard <julien.eluard@gmail.com>
2013-01-20 23:13:55 -0300

    Moved to the right package.

--- core/src/test/java/com/github/jeluard/stone/api/TimeSeriesTest.java
+++ core/src/test/java/com/github/jeluard/stone/api/TimeSeriesTest.java
@@ -14,10 +14,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.github.jeluard.stone.spi;
+package com.github.jeluard.stone.api;
 
-import com.github.jeluard.stone.api.Archive;
-import com.github.jeluard.stone.api.TimeSeries;
+import com.github.jeluard.stone.spi.Storage;
+import com.github.jeluard.stone.spi.StorageFactory;
 import com.google.common.base.Optional;
 
 import java.io.IOException;
