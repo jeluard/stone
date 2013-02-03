5834ff8d33dc9c7a0184df308077b3883f15a526
Julien Eluard <julien.eluard@gmail.com>
2013-02-03 14:58:10 -0300

    Cleanup.

--- core/src/main/java/com/github/jeluard/stone/api/Window.java
+++ core/src/main/java/com/github/jeluard/stone/api/Window.java
@@ -18,9 +18,8 @@ package com.github.jeluard.stone.api;
 
 import com.google.common.base.Optional;
 import com.google.common.base.Preconditions;
-import java.util.ArrayList;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
