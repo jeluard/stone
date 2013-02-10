97c4f68462a31d77a341ae4945eb245867151c5f
Julien Eluard <julien.eluard@gmail.com>
2013-02-10 16:20:06 -0300

    Fixed off-by-one iteration.

--- implementations/storages/memory/src/main/java/com/github/jeluard/stone/storage/memory/MemoryStorage.java
+++ implementations/storages/memory/src/main/java/com/github/jeluard/stone/storage/memory/MemoryStorage.java
@@ -1,11 +1,12 @@
-/*
- * Copyright 2013 julien.
+/**
+ * Copyright 2012 Julien Eluard
+ * This project includes software developed by Julien Eluard: https://github.com/jeluard/
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -25,35 +26,39 @@ import java.util.concurrent.atomic.AtomicIntegerArray;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.AtomicLongArray;
 
+import org.joda.time.Duration;
+
 /**
  * An in-memory {@link Storage} implementation.
  */
 public final class MemoryStorage extends Storage {
 
   private final int maxSize;
-  private final int nbConsolidators;
+  private final int nbValues;
   private final AtomicLong index = new AtomicLong(0);
   private final AtomicLongArray timestamps;
   private final AtomicIntegerArray values;
 
-  public MemoryStorage(final int maxSize, final int nbConsolidators) {
+  public MemoryStorage(final Duration duration, final int maxSize, final int nbValues) {
+    super(duration, maxSize, nbValues);
+
     this.maxSize = maxSize;
-    this.nbConsolidators = nbConsolidators;
+    this.nbValues = nbValues;
     this.timestamps = new AtomicLongArray(maxSize);
-    this.values = new AtomicIntegerArray(maxSize*nbConsolidators);
+    this.values = new AtomicIntegerArray(maxSize*nbValues);
   }
 
   @Override
   public Iterable<Pair<Long, int[]>> all() throws IOException {
     final List<Pair<Long, int[]>> all = new ArrayList<Pair<Long, int[]>>(this.maxSize);
-    for (int i = 0; i < this.maxSize-1; i++) {
+    for (int i = 0; i < this.maxSize; i++) {
       final long timestamp = this.timestamps.get(i);
       if (timestamp == 0L) {
         break;
       }
-      final int[] consolidates = new int[this.nbConsolidators];
-      for (int j = 0; j < this.nbConsolidators; j++) {
-        consolidates[j] = this.values.get(i*this.nbConsolidators+j);
+      final int[] consolidates = new int[this.nbValues];
+      for (int j = 0; j < this.nbValues; j++) {
+        consolidates[j] = this.values.get(i*this.nbValues+j);
       }
       all.add(new Pair<Long, int[]>(timestamp, consolidates));
     }
