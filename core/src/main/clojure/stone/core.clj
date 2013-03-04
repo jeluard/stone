50567b95e08383036b1837c1c088e514317e21b8
Julien Eluard <julien.eluard@gmail.com>
2013-03-04 00:27:46 -0300

    Improved clojure API.

--- core/src/main/clojure/stone/core.clj
+++ core/src/main/clojure/stone/core.clj
@@ -1,10 +1,12 @@
 (ns stone.core
-  (:import (com.github.jeluard.stone.api Listener TimeSeries)
+  (:import (com.github.jeluard.stone.api ConsolidationListener Listener Reader TimeSeries Window WindowedTimeSeries)
+           (com.github.jeluard.stone.spi Storage)
+           (com.github.jeluard.stone.helper Loggers Storages)
            (com.google.common.base Optional)))
 
 (set! *warn-on-reflection* true)
 
-(defn -wrap-fn-as-listener [fn]
+(defn -wrap-as-listener [fn]
   (reify Listener
     (onPublication [this previousTimestamp currentTimestamp value]
       (fn previousTimestamp currentTimestamp value))))
@@ -12,7 +14,41 @@
 (defn create-ts
   ([id listeners dispatcher] (create-ts id 1 listeners dispatcher))
   ([id granularity listeners dispatcher] (create-ts id granularity (Optional/absent) listeners dispatcher))
-  ([id granularity latestTimestamp listeners dispatcher] (TimeSeries. id granularity latestTimestamp (map -wrap-fn-as-listener listeners) dispatcher)))
+  ([id granularity latestTimestamp listeners dispatcher] (TimeSeries. id granularity latestTimestamp (map -wrap-as-listener listeners) dispatcher)))
+
+(defn beginning [^Reader reader]
+  (.get (.beginning reader)))
+
+(defn end [^Reader reader]
+  (.get (.beginning reader)))
+
+(defn all [^Reader reader]
+  (.all reader))
+
+(defn during [^Reader reader before after]
+  (.during reader before after))
+
+(defn -wrap-fn-as-consolidation-listener [fn]
+  (reify ConsolidationListener
+    (onConsolidation [this timestamp consolidates]
+      (fn timestamp (vec consolidates)))))
+
+(defn -wrap-storage-as-consolidation-listener
+  ([storage] (-wrap-storage-as-consolidation-listener storage Loggers/BASE_LOGGER))
+  ([storage logger] (Storages/asConsolidationListener storage logger)))
+
+(defn -wrap-as-consolidation-listener [x]
+  (if (instance? Storage x)
+    (-wrap-storage-as-consolidation-listener x)
+    (-wrap-fn-as-consolidation-listener x)))
+
+(defn window
+  ([size consolidatorTypes] (window size consolidatorTypes '()))
+  ([size consolidatorTypes consolidationListeners] (Window. size consolidatorTypes (map -wrap-as-consolidation-listener consolidationListeners))))
+
+(defn create-windowed-ts
+  ([id windows dispatcher] (create-windowed-ts id 1 windows dispatcher))
+  ([id granularity windows dispatcher] (WindowedTimeSeries. id granularity windows dispatcher)))
 
 (defn publish [^TimeSeries ts timestamp value]
   (.publish ts timestamp value))
--- examples/src/test/clojure/stone/examples.clj
+++ examples/src/test/clojure/stone/examples.clj
@@ -1,11 +1,35 @@
 (ns stone.examples
   (:use [stone.core :as st])
-  (:import (com.github.jeluard.stone.dispatcher.sequential SequentialDispatcher)))
+  (:import
+    (com.github.jeluard.stone.consolidator MaxConsolidator MinConsolidator)
+    (com.github.jeluard.stone.dispatcher.sequential SequentialDispatcher)
+    (com.github.jeluard.stone.storage.memory MemoryStorage)))
 
 (def dispatcher (SequentialDispatcher.))
 
-(def ts (st/create-ts "timeseries" (list (fn [a b c] (print (str "Got value " c)))) dispatcher))
+(def ts (st/create-ts "timeseries" (list (fn [a b c] (println (str "Got value " c)))) dispatcher))
 
 (st/publish ts 123 1)
 
 (st/close ts)
+
+;;
+
+(def storage (MemoryStorage. 1000))
+
+(def windows (list (window 3 (list MaxConsolidator MinConsolidator)
+                             (list storage (fn [a b] (println (str "Got consolidates " b)))))))
+
+(def wts (st/create-windowed-ts "windowed-timeseries" windows dispatcher))
+
+(def now (System/currentTimeMillis))
+(st/publish wts now 1)
+(st/publish wts (+ now 1) 2)
+(st/publish wts (+ now 2) 3)
+(st/publish wts (+ now 3) 4)
+
+(println (take 1 (st/all storage)))
+(println (str "First timestamp stored " (st/end storage)))
+(println (str "Latest timestamp stored " (st/end storage)))
+
+(st/close wts)
