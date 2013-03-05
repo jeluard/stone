baf6d4733120b0008508e5e12e549c754175796a
Julien Eluard <julien.eluard@gmail.com>
2013-03-05 00:24:56 -0300

    Sort values before returning.

--- implementations/storages/memory/src/main/java/com/github/jeluard/stone/storage/memory/MemoryStorage.java
+++ implementations/storages/memory/src/main/java/com/github/jeluard/stone/storage/memory/MemoryStorage.java
@@ -18,9 +18,12 @@ package com.github.jeluard.stone.storage.memory;
 
 import com.github.jeluard.guayaba.base.Pair;
 import com.github.jeluard.stone.spi.Storage;
+import com.google.common.primitives.Longs;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.AtomicLongArray;
@@ -54,6 +57,12 @@ public final class MemoryStorage extends Storage {
 
       all.add(new Pair<Long, int[]>(timestamp, this.values.get(i)));
     }
+    Collections.sort(all, new Comparator<Pair<Long, int[]>>() {
+      @Override
+      public int compare(final Pair<Long, int[]> pair1, Pair<Long, int[]> pair2) {
+        return Longs.compare(pair1.first, pair2.first);
+      }
+    });
     return all;
   }
 
