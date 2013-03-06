aff94e2d28baaadb1001aba6230cef741c0a9da5
Julien Eluard <julien.eluard@gmail.com>
2013-03-06 00:41:17 -0300

    Added some tests.

--- core/src/test/java/com/github/jeluard/stone/api/WindowedTimeSeriesTest.java
+++ core/src/test/java/com/github/jeluard/stone/api/WindowedTimeSeriesTest.java
@@ -16,8 +16,84 @@
  */
 package com.github.jeluard.stone.api;
 
+import com.github.jeluard.stone.consolidator.MaxConsolidator;
+import com.github.jeluard.stone.spi.Dispatcher;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
 public class WindowedTimeSeriesTest {
 
-  
+  private static class DumbDispatcher extends Dispatcher {
+    public DumbDispatcher() {
+      super(Dispatcher.DEFAULT_EXCEPTION_HANDLER);
+    }
+    @Override
+    public boolean dispatch(long previousTimestamp, long currentTimestamp, int value, Listener[] listeners) {
+      for (final Listener listener : listeners) {
+        listener.onPublication(previousTimestamp, currentTimestamp, value);
+      }
+      return true;
+    }
+  }
+
+  @Test
+  public void shouldWindowWithoutListenerDoNothing() throws IOException {
+    final Window window = Window.of(10).consolidatedBy(MaxConsolidator.class);
+    final WindowedTimeSeries timeSeries = new WindowedTimeSeries("id", 1, Arrays.asList(window), new DumbDispatcher());
+    timeSeries.publish(1, 1);
+    timeSeries.close();
+  }
+
+  @Test
+  public void shouldConsolidationBeTriggeredWhenLastElementOfWindowIsPublished() throws IOException {
+    final ConsolidationListener consolidationListener = Mockito.mock(ConsolidationListener.class);
+    final Window window = Window.of(2).listenedBy(consolidationListener).consolidatedBy(MaxConsolidator.class);
+    final WindowedTimeSeries timeSeries = new WindowedTimeSeries("id", 1, Arrays.asList(window), new DumbDispatcher());
+    timeSeries.publish(1, 1);
+    timeSeries.publish(2, 1);
+    timeSeries.close();
+
+    Mockito.verify(consolidationListener).onConsolidation(Mockito.anyLong(), Mockito.<int[]>any());
+  }
+
+  @Test
+  public void shouldConsolidationBeTriggeredWhenLastElementOfWindow2IsPublished() throws IOException {
+    final ConsolidationListener consolidationListener = Mockito.mock(ConsolidationListener.class);
+    final Window window = Window.of(3).listenedBy(consolidationListener).consolidatedBy(MaxConsolidator.class);
+    final WindowedTimeSeries timeSeries = new WindowedTimeSeries("id", 1, Arrays.asList(window), new DumbDispatcher());
+    timeSeries.publish(1, 1);
+    timeSeries.publish(3, 1);
+    timeSeries.close();
+
+    Mockito.verify(consolidationListener).onConsolidation(Mockito.anyLong(), Mockito.<int[]>any());
+  }
+
+  @Test
+  public void shouldConsolidationBeTriggeredWhenElementOfNewWindowIsPublished() throws IOException {
+    final ConsolidationListener consolidationListener = Mockito.mock(ConsolidationListener.class);
+    final Window window = Window.of(3).listenedBy(consolidationListener).consolidatedBy(MaxConsolidator.class);
+    final WindowedTimeSeries timeSeries = new WindowedTimeSeries("id", 1, Arrays.asList(window), new DumbDispatcher());
+    timeSeries.publish(1, 1);
+    timeSeries.publish(7, 1);
+    timeSeries.close();
+
+    Mockito.verify(consolidationListener).onConsolidation(Mockito.anyLong(), Mockito.<int[]>any());
+  }
+
+  @Test
+  public void shouldConsolidationBeTriggeredTwiceWhenLastElementOfNewWindowIsPublished() throws IOException {
+    final ConsolidationListener consolidationListener = Mockito.mock(ConsolidationListener.class);
+    final Window window = Window.of(3).listenedBy(consolidationListener).consolidatedBy(MaxConsolidator.class);
+    final WindowedTimeSeries timeSeries = new WindowedTimeSeries("id", 1, Arrays.asList(window), new DumbDispatcher());
+    timeSeries.publish(1, 1);
+    timeSeries.publish(8, 1);
+    timeSeries.close();
+
+    Mockito.verify(consolidationListener, Mockito.times(2)).onConsolidation(Mockito.anyLong(), Mockito.<int[]>any());
+  }
 
 }
